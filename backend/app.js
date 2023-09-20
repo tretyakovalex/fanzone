23ea2dcadc15daa987f464f51a4ed3c4db131c81
tretyakovalex <tretyakovalexmax@gmail.com>
2023-09-20 15:34:33 +0400

    added frontend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
 const dbConnection = require('./configs/mongodb');
 
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+
+// Serve static files from the Svelte build folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Handle all other routes by serving the Svelte app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
